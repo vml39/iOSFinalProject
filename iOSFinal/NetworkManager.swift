a414e7cc1279b142e5c01d5651fc906403caf399
Vivian Li <31635435+vml39@users.noreply.github.com>
2018-05-05 16:59:15 -0400

    Update NetworkManager.swift

--- iOSFinal/NetworkManager.swift
+++ iOSFinal/NetworkManager.swift
@@ -13,7 +13,7 @@ import SwiftyJSON
 class NetworkManager {
     
     /// Endpoint URL string
-    //1private static let endpoint = "https://mindylou.github.io/classes.json"
+    // TODO: get endpoint: private static let endpoint = "https://mindylou.github.io/classes.json"
     
     /**
      Gets classes from the endpoint.
@@ -24,32 +24,32 @@ class NetworkManager {
      @escaping means that you can jump out of the function and call the function passed in.
      This function is called before the function returns, or it "escapes" from the function.
      */
-    //2static func getClasses(completion: @escaping ([Profile]) -> Void) {
+    static func getClasses(completion: @escaping ([Profile]) -> Void) {
         // Here, we make a GET request from the endpoint. This means that we are
         // requesting data. A POST request submits data to the endpoint to be processed.
         // We validate it so that the response code is 200 (OK), so we know we didn't receive
         // a 404 (not found)/request error.
-        //3Alamofire.request(endpoint, method: .get)
-            //4.validate().responseJSON { response in
+        Alamofire.request(endpoint, method: .get)
+            Alamofire.validate().responseJSON { response in
                 // Depending on what response JSON we get here,
                 // we can appropriately handle it.
-                //5switch response.result {
-                //6case let .success(data):
-                   //7 let classJSONDictionary = JSON(data)["data"].dictionaryValue
-                    //8if let classJSONArray = classJSONDictionary["classes"]?.arrayValue {
-                        //9var classArray = [Profile]()
-                        //10for classJSON in classJSONArray {
-                            //11classArray.append(Profile(json: classJSON))
-                        //12}
+                switch response.result {
+                case let .success(data):
+                   let profileJSONDictionary = JSON(data)["data"].dictionaryValue
+                    if let profileJSONArray = profileJSONDictionary["profiles"]?.arrayValue {
+                        var profileArray = [Profile]()
+                        for profileJSON in profileJSONArray {
+                            profileArray.append(Profile(json: profileJSON))
+                        }
                         
                         // Call the function we passed in to do whatever we want
                         // to the classArray (eg. show it on screen)
-                        //13completion(classArray)
-                    //14}
-                //15case .failure(let error):
-                    //16print(error.localizedDescription)
-                //17}
-        //18}
-    //19}
+                        completion(profileArray)
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+        }
+    }
 }
 
